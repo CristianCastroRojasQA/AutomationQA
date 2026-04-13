6303d822140c090b27a0b0a4bd92da3e656ee70b
Cristian Castro Rojas <cristian.castrorojas@evertecinc.com>
2026-04-13 16:27:45 -0500

    refactor: añade logging y renombra TecnologiaFlow a TecnologiasFlow

---
+++ flows/configuracion/adquirente/tecnologias_flow.py
@@ -0,0 +1,20 @@
+from flows.configuracion.adquirente.base_catalogo_flow import BaseCatalogoFlow
+from pages.configuracion.adquirente.tecnologia_page import TecnologiaPage
+from utils.logger import get_logger
+
+
+class TecnologiasFlow(BaseCatalogoFlow):
+    """
+    Flow funcional para la pantalla de Tecnologia (ABCUC022).
+    Hereda todos los flujos genéricos desde BaseCatalogoFlow.
+    """
+
+    def __init__(self, page):
+        logger = get_logger(self.__class__.__name__)
+        logger.debug("Inicializando TecnologíasFlow para la pantalla ABCUC022.")
+
+        logger.info("=== COMPONENTE: CATÁLOGO DE TECNOLOGÍAS (ABCUC022) ===")
+
+        super().__init__(page, TecnologiaPage(page))
+
+        logger.debug("TecnologiasPage vinculada exitosamente con el driver de Playwright.")
